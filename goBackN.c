8fc25343b68ec3ca72ffb34e6a268bd7deda9276
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2024-11-02 00:44:47 +0530

    Updated go backN

--- goBackN.c
+++ goBackN.c
@@ -3,31 +3,62 @@
 Sample
 
 */
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <time.h>
+
+#define WINDOW_SIZE 4      
+#define TOTAL_FRAMES 10    
+
+void sender();
+void receiver(int frame);
+
+int next_frame_to_send = 0;    
+int ack_received = -1;         
+bool frame_acknowledged[TOTAL_FRAMES] = { false}; 
+
 int main(){
-    int k; // window size
-    printf("Enter window size : ");
-    scanf("%d",&k);
     
-    int noOfFrames;
-    printf("Enter number of frames to transmit: ");
-    scanf("%d",&noOfFrames);
-    
-    // create a array of frames
-    int frames[noOfFrames];
-    printf("Enter %d frames:\n",noOfFrames);
-    for(int i = 0 ; i < noOfFrames ; i++){
-        scanf("%d",&frames[i]);
-    }
-    
-    printf("With Go Back N protocol the frames will be sent in the following manner (assuming no corruption of frames)\n");
-    
-    printf("\nAfter sending %d frames at each stage sender waits for acknowledgement sent by the receiver\n\n",k);
-    
-    for(int i = 0 ; i < noOfFrames ; i+=k){
-        for(int j = i ; j < i + k; j++){
-            if(j < noOfFrames) printf("%d ",frames[j]);
+    srand(time(0));  
+    printf("Starting Go-Back-N Protocol Simulation...\n\n");
+    sender();
+    printf("\nSimulation complete.\n");
+    return 0;
+
+}
+void sender(){
+    while (ack_received < TOTAL_FRAMES - 1){
+        int frames_in_window = 0;
+        for (int i = next_frame_to_send; i < next_frame_to_send + WINDOW_SIZE && i < TOTAL_FRAMES; i++){
+            if (!frame_acknowledged[i]){
+                printf("Sender: Sending Frame %d\n", i);
+                receiver(i);
+                frames_in_window++;
+            }
         }
-        printf("\nAcknowledgement of above frames sent is received by sender\n\n");                      
+        int ack = next_frame_to_send + (rand() % frames_in_window);
+        printf("Sender: Acknowledgment received for Frame %d\n", ack);
+        if (ack > ack_received){
+            for (int j = ack_received + 1; j <= ack; j++){
+                frame_acknowledged[j] = true;
+            }
+            ack_received = ack;
+            next_frame_to_send = ack + 1;
+        }
+        else{
+            printf("Sender: No new acknowledgment, resending frames from %d\n", next_frame_to_send);
+        }
+        usleep(500000); 
+    }
+}
+void receiver(int frame){
+    int rand_failure = rand() % 5;  
+    if (rand_failure == 0){
+        printf("Receiver: Frame %d lost (simulated)\n", frame);
+    }
+    else{
+        printf("Receiver: Frame %d received\n", frame);
     }
 }
