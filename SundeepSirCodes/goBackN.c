c384ea14f61b8ce5b0961e4b62a3d8691c668fe5
Rangaraju Viishhnu <viishhnu2005@gmail.com>
2024-11-02 21:49:45 +0530

    Updated codes

--- SundeepSirCodes/goBackN.c
+++ SundeepSirCodes/goBackN.c
@@ -1,31 +1,30 @@
+/*Develop a simple data link layer that performs the flow control using the Go Back N protocol in   “c”
+
+Sample
+
+*/
 #include<stdio.h>
-
-int main(){
-	int n,f,frames[30],i;
-	printf("Enter window size : ");
-	scanf("%d",&n);
-	printf("Enter number of frames to transmit: ");
-	scanf("%d",&f);
-	printf("Enter %d frames: \n",f);
-	for(i=1;i<=f;i++){
-		scanf("%d",&frames[i]);
-	}
-	printf("With sliding window protocol the frames will be sent in the following manner (assuming no corruption of frames)\n");
-	printf("After sending %d frames at each stage sender waits for acknowledgement sent by the receiver\n",n);
-
-	for(i=1;i<=f;i++){
-		if(i%n==0){
-			printf("%d ",frames[i]);
-			printf("Acknowledgement of above frames sent is received by sender\n");
-
+int main()
+{
+    int window=0;
+	printf("enter Window size : ");
+	scanf("%d",&window);
+	int sent,ack,i=0;
+	while(1){
+		for(i=0;i<window;i++){
+			printf("frame Transmitted %d \n",sent);
+			sent++;
+			if(sent==window){
+				break;
+			}
+		}
+		printf("enter last received acknowledgment : ");
+		scanf("%d",&ack);
+		if(ack==window){
+			break;
 		}
 		else{
-			//printf("recieved by sender\n");
-			printf("%d ",frames[i]);
-		}
-	}
-	if(f%n!=0){
-	printf("Acknowledgement of above frames sent is received by sender\n");
-
+			sent = ack;
+			}
 	}
 }
